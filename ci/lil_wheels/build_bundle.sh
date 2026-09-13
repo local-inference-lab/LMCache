4b7fa20977e9af8fb85196cf2970e19f062e6129
Martin Vit <martin@voipmonitor.org>
2026-09-13 16:47:31 +0000

    fix(ci): inspect LMCache wheel contents without SIGPIPE
    Signed-off-by: Martin Vit <martin@voipmonitor.org>

--- ci/lil_wheels/build_bundle.sh
+++ ci/lil_wheels/build_bundle.sh
@@ -47,7 +47,8 @@ metadata=$(unzip -p "${wheel}" '*/METADATA')
 package_name=$(awk -F': ' '$1 == "Name" {print $2; exit}' <<<"${metadata}")
 package_version=$(awk -F': ' '$1 == "Version" {print $2; exit}' <<<"${metadata}")
 test "${package_name}" = lmcache
-unzip -l "${wheel}" | grep -Eq 'lmcache/cuda_ops\.[^/]*\.so$'
+wheel_listing=$(unzip -Z1 "${wheel}")
+grep -Eq '^lmcache/cuda_ops\.[^/]*\.so$' <<<"${wheel_listing}"
 digest=$(sha256sum "${wheel}" | awk '{print $1}')
 file=$(basename "${wheel}")
 url="https://github.com/${repository}/releases/download/${release_tag}/${file}"
